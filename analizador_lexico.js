79903f4b925e6d901aa85d70735a58e776253359
Leonardo Covarrubias Lemus <leonardolemus216@gmail.com>
2024-02-25 01:22:07 -0600

    Lexema Funcionando

--- analizador_lexico.js
+++ analizador_lexico.js
@@ -1,3 +1,4 @@
+
 function analizar() {
     var codigo = document.querySelector("#codigo textarea").value;
     var tabla = document.getElementById("analizar_lexico");
@@ -7,10 +8,21 @@ function analizar() {
     var id = 1;
 
     // Definir palabras reservadas
-    var palabrasReservadas = ["if", "else", "while", "for", "function", "var", "let", "const"];
+    var palabrasReservadas = [
+        "abstract", "continue", "for", "new", "switch",
+        "assert", "default", "goto", "package", "synchronized",
+        "boolean", "do", "if", "private", "this",
+        "break", "double", "implements", "protected", "throw",
+        "byte", "else", "import", "public", "throws",
+        "case", "enum", "instanceof", "return", "transient",
+        "catch", "extends", "int", "short", "try",
+        "char", "final", "interface", "static", "void",
+        "class", "finally", "long", "strictfp", "volatile",
+        "const", "float", "native", "super", "while"
+    ];
 
     // Ejecutar análisis léxico
-    lineas.forEach(function(linea, indiceLinea) {
+    lineas.forEach(function (linea, indiceLinea) {
         var columnaActual = 1;
         var lexema = "";
         var tipo = "";
@@ -26,55 +38,66 @@ function analizar() {
                     tipo = "";
                 }
                 columnaActual++;
-            } else {
-                if (caracter.match(/[a-zA-Z_]/)) {
-                    lexema += caracter;
-                    tipo = palabrasReservadas.includes(lexema) ? 'RESERVADA' : 'IDENTIFICADOR';
-                } else if (caracter.match(/\d/)) {
-                    lexema += caracter;
-                    tipo = 'NUMERO';
-                    // Verificar si el próximo carácter no es un dígito o un punto
-                    if (i === linea.length - 1 || (!linea[i + 1].match(/\d/) && linea[i + 1] !== '.')) {
-                        agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual);
-                        id++;
-                        lexema = "";
-                        tipo = "";
-                    }
-                } else if (caracter === '.') {
-                    // Verificar si el lexema actual es un número con decimales (double o float)
-                    if (tipo === 'NUMERO' && linea[i + 1].match(/\d/)) {
-                        lexema += caracter;
-                        tipo = 'DOUBLE';
-                    } else {
-                        // Tratar el punto como un operador si no se sigue de un dígito
-                        lexema = caracter;
-                        tipo = 'OPERADOR';
-                        agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual);
-                        id++;
-                        lexema = "";
-                        tipo = "";
-                    }
-                } else if (caracter.match(/[\;\+\-\*\/\=\;]/)) {
-                    lexema = caracter;
-                    tipo = 'OPERADOR';
+            } else if (caracter === ';' || caracter === '{' || caracter === '}') { // Delimitadores
+                if (lexema !== "") {
+                    agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual);
+                    id++;
+                }
+                lexema = caracter;
+                tipo = "DELIMITADOR";
+                agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual); // Incrementa columna solo para delimitadores
+                id++;
+                lexema = "";
+                tipo = "";
+                columnaActual++; // Incrementa columna
+            } else if (caracter === '/') { // Comentarios
+                if (lexema !== "") {
+                    agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual);
+                    id++;
+                }
+                lexema = caracter;
+                tipo = "COMENTARIO";
+                agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual);
+                id++;
+                lexema = "";
+                tipo = "";
+                columnaActual++;
+            } else if (caracter.match(/[a-zA-Z_]/)) { // Identificadores y palabras reservadas
+                lexema += caracter;
+                tipo = palabrasReservadas.includes(lexema) ? 'PALABRA_RESERVADA' : 'IDENTIFICADOR';
+            } else if (caracter.match(/\d/)) { // Números
+                lexema += caracter;
+                tipo = 'NUMERO';
+                // Verificar si el próximo carácter no es un dígito o un punto
+                if (i === linea.length - 1 || (!linea[i + 1].match(/\d/) && linea[i + 1] !== '.')) {
                     agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual);
                     id++;
                     lexema = "";
                     tipo = "";
-                } else {
-                    lexema += caracter;
-                    tipo = 'DESCONOCIDO';
                 }
-                if (i === linea.length - 1 || linea[i + 1].match(/\s/)) {
-                    // Si el próximo carácter es espacio en blanco, agregar lexema a la tabla
-                    if (lexema !== "") {
-                        agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual);
-                        id++;
-                        lexema = "";
-                        tipo = "";
-                    }
-                }
+            } else if (caracter.match(/[\+\-\*\/\=]/)) { // Operadores
+                lexema = caracter;
+                tipo = 'OPERADOR';
+                agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual);
+                id++;
+                lexema = "";
+                tipo = "";
+            } else { // Caracteres desconocidos
+                // Encontró un carácter no válido, detenerse y enviar los caracteres acumulados al analizador léxico
+                agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual - lexema.length);
+                id++;
+                lexema = "";
+                tipo = "";
+                i--; // Volver a leer el carácter actual en el próximo ciclo (ya que no pertenecía al token)
             }
+        }
+
+        // Enviar los caracteres restantes al analizador léxico
+        if (lexema !== "") {
+            agregarTokenATabla(id, lexema, tipo, indiceLinea + 1, columnaActual - lexema.length);
+            id++;
+            lexema = "";
+            tipo = "";
         }
     });
 
